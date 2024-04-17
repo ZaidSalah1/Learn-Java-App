0dbc4f6d886f6374fb404fff056bd117b31542dd
zaidsalah123er <zaid71457@gmail.com>
2024-04-17 08:25:49 +0200

    Learn Java App

--- app/src/main/java/com/example/learnjavaapp/Activites/DetaildActivity.java
+++ app/src/main/java/com/example/learnjavaapp/Activites/DetaildActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.learnjavaapp.R;
 
@@ -29,6 +30,42 @@ public class DetaildActivity extends AppCompatActivity {
         }else if (intent.getIntExtra("index",0) == 4){
             setContentView(R.layout.level_variables);
         }
+    }
 
+    // Lifecycle methods
+    @Override
+    protected void onStart() {
+        super.onStart();
+      //  Toast.makeText(this, "On Start", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+     //   Toast.makeText(this, "On Resume", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+     //   Toast.makeText(this, "On Pause", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        //Toast.makeText(this, "On Stop", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+       // Toast.makeText(this, "On Destroy", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+     //   Toast.makeText(this, "On Restart", Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/learnjavaapp/Activites/MainActivity.java
+++ app/src/main/java/com/example/learnjavaapp/Activites/MainActivity.java
@@ -73,37 +73,37 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        Toast.makeText(this, "On Start", Toast.LENGTH_SHORT).show();
+     //   Toast.makeText(this, "On Start", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Toast.makeText(this, "On Resume", Toast.LENGTH_SHORT).show();
+      //  Toast.makeText(this, "On Resume", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Toast.makeText(this, "On Pause", Toast.LENGTH_SHORT).show();
+     //   Toast.makeText(this, "On Pause", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Toast.makeText(this, "On Stop", Toast.LENGTH_SHORT).show();
+      //  Toast.makeText(this, "On Stop", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Toast.makeText(this, "On Destroy", Toast.LENGTH_SHORT).show();
+     //   Toast.makeText(this, "On Destroy", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        Toast.makeText(this, "On Restart", Toast.LENGTH_SHORT).show();
+   //     Toast.makeText(this, "On Restart", Toast.LENGTH_SHORT).show();
     }
 
 
